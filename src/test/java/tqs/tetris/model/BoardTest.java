14e7804c9d7f68e3b92fda296469f20317f718e9
Yenifer <yeniferwuzhang@gmail.com>
2025-10-15 18:16:19 +0200

    refactor: board test

--- src/test/java/tqs/tetris/model/BoardTest.java
+++ src/test/java/tqs/tetris/model/BoardTest.java
@@ -17,16 +17,15 @@ import org.junit.jupiter.api.Test;
  */
 
 public class BoardTest {
-    private Board board;
+    Board board;
 
     @BeforeEach
-    void setUp() {
-        board = new Board(10, 20);
+    void setUp() throws Exception {
+        board = new Board(10,20);
     }
 
     @Test
     void boardInicialization() {
-        Board board = new Board(10,20);
         assertEquals(10, board.getWidth());
         assertEquals(20, board.getHeight());
     }
@@ -44,7 +43,6 @@ public class BoardTest {
     
     @Test
     void boardEmptyCells() {
-        Board board = new Board(10,20);
         for(int y = 0; y<board.getHeight(); y++) {
             for(int x = 0; x<board.getWidth(); x++) {
                 assertTrue(board.isEmpty(x,y));
@@ -54,7 +52,6 @@ public class BoardTest {
 
     @Test
     void boardPlaceTetromino() {
-        Board board = new Board(10,20);
         Tetromino tetromino = new Tetromino(TetrominoType.I);
         tetromino.setPosition(4, 0); // Place at the top center
         board.placeTetromino(tetromino);
@@ -66,37 +63,34 @@ public class BoardTest {
 
     @Test
     void boardCollisions() {
-        Board b = new Board(10,20);
         Tetromino t1 = Tetromino.ofType(TetrominoType.I);
         t1.setPosition(0, 0);
-        assertTrue(b.collides(t1.moveDown(-1,0)));
+        assertTrue(board.collides(t1.moveDown(-1,0)));
     }
 
     @Test
     void setAndGetCell() {
-        Board b = new Board(5,5);
         // initially empty
-        assertTrue(b.isEmpty(2,2));
+        assertTrue(board.isEmpty(2,2));
 
         // occupy then free
-        b.setCell(2,2, true);
-        assertFalse(b.isEmpty(2,2));
-        b.setCell(2,2, false);
-        assertTrue(b.isEmpty(2,2));
+        board.setCell(2,2, true);
+        assertFalse(board.isEmpty(2,2));
+        board.setCell(2,2, false);
+        assertTrue(board.isEmpty(2,2));
     }
 
     @Test
     void clearLines() {
-        Board b = new Board(4,4);
         // fill the bottom row (y=0)
-        for (int x = 0; x < b.getWidth(); x++) {
-            b.setCell(x, 0, true);
+        for (int x = 0; x < board.getWidth(); x++) {
+            board.setCell(x, 0, true);
         }
-        int cleared = b.clearLines();
+        int cleared = board.clearLines();
         assertEquals(1, cleared);
         // should be empty
-        for (int x = 0; x < b.getWidth(); x++) {
-            assertTrue(b.isEmpty(x, 0));
+        for (int x = 0; x < board.getWidth(); x++) {
+            assertTrue(board.isEmpty(x, 0));
         }
     }
 }

0021ea9ee5e45e17452ee71fa2aaed644211d3ab
grif65 <griflol27@gmail.com>
2025-10-26 12:13:40 +0100

    test: fixed values tetramino creaton

--- src/test/java/tqs/tetris/model/TetraminoFactoryTest.java
+++ src/test/java/tqs/tetris/model/TetraminoFactoryTest.java
@@ -1,6 +1,7 @@
 package tqs.tetris.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class TetraminoFactoryTest {
@@ -20,5 +21,40 @@ public class TetraminoFactoryTest {
         mockRandom mockRandom2 = new mockRandom(2, 2);
         factory.setRandom(mockRandom2);
         assertEquals(mockRandom2, factory.getRandom());
+    }
+
+    @Test
+    public void createTetraminoTest() {
+        tetraminoFactory factory = new tetraminoFactory();
+        tetramino I = factory.createTetramino('I', 0);
+        assertEquals("I", I.getShape());
+        assertEquals(0, I.getColor());
+
+        // Pairwise done with: https://pairwise.teremokgames.com/
+        /* gives the 8 * 9 = 72 combinations but it dosn't make sense to test all of them
+         * shapes: I, O, T, S, Z, J, L -> I, O, T, S, Z, J, L, X
+         * colors: 0, 1, 2, 3, 4, 5 -> MIN, -1, 0, 1, 3 , 4, 5, 6, MAX
+         * Sense these values don't interact with each other and they are already teste in
+         * tetraminoTest, we will just test a subset of the combinations here.
+         */
+
+        tetramino O = factory.createTetramino('O', 3);
+        assertEquals("O", O.getShape());
+        assertEquals(3, O.getColor());
+
+        tetramino T = factory.createTetramino('T', 5);
+        assertEquals("T", T.getShape());
+        assertEquals(5, T.getColor());
+        // Invalid shape
+        try {
+            factory.createTetramino('X', 3);
+            assertTrue(false);
+        } catch (Exception e) { }
+        // Invalid color
+        try {
+            factory.createTetramino('I', 6);
+            assertTrue(false);
+        } catch (Exception e) { }
+
     }
 }
